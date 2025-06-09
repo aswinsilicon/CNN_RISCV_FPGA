7d189e8640acbfb46b6b79144eb45a9daa6ce277
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-06-09 17:38:42 +0530

    Update alu.v

--- RISC_V_pipelined/alu.v
+++ RISC_V_pipelined/alu.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/08/2025 08:36:30 PM
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 /* 
    ALU Unit
