bfe1ca59570c0fe91b88c38c0438d1bd0ea2eb21
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-06-09 17:39:01 +0530

    Update alu_decoder.v

--- RISC_V_pipelined/alu_decoder.v
+++ RISC_V_pipelined/alu_decoder.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/08/2025 11:01:59 PM
-// Design Name: 
-// Module Name: alu_decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 /*
    ALU Decoder
